89f3fdbd60702cc72cc04cf1eb1e366d4cf8f474
DikzNakano <dikzpreset11@gmail.com>
2026-06-08 12:48:31 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-console.log("DikzNakano Loaded");
-
-document.querySelectorAll("a").forEach(btn => {
-  btn.addEventListener("click", e => {
-    if(btn.getAttribute("href") === "#"){
-      e.preventDefault();
-    }
-  });
-});
